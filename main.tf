cef864f6143b2955c9365842c077bc2be2dd92f4
John Noss <john.noss@runkeeper.com>
2017-12-06 16:59:16 -0500

    use Terraform 0.11 module provider inheritance

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 # MAIN
 
-provider "aws" {
-  alias   = "provided"
-  profile = "${var.aws_profile}"
-  region  = "${var.region}"
-}
-
 data "aws_region" "region" {
   name = "${var.region}"
 }
 
 data "aws_vpc" "vpc" {
-  provider = "aws.provided"
   id = "${var.vpc_id}"
 }
